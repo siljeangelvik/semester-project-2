872a7ad5491eead018eba51bb916f67f242bb178
silje <angelviksilje@gmail.com>
2022-11-23 16:54:30 +0100

    testing some things

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-import './src/css/style.css';
-import './src/css/index.css';
-import javascriptLogo from './src/assets/javascript.svg';
-import {setupCounter} from './src/js/counter.js';
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-   <!-- <a href="https://vitejs.dev" target="_blank">
+    <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a> -->
+    </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
-    <p><a href="./index.html">Index HTML</a></p>
-    <p><a href="./resources/ganttchart/ganttchart.html">Gantt Chart</a></p>
-
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -22,6 +18,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`;
+`
 
 setupCounter(document.querySelector('#counter'))
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+// import { sassPlugin }
+export default defineConfig({
+    plugins: []
+})
