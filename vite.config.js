b0051a20be55fe951a78f4955a4847dd33d7dae3
silje <angelviksilje@gmail.com>
2022-11-23 18:55:03 +0100

    testing some things

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "./",
+    base: '/semester-project-2/',
+    root: path.resolve(__dirname, "./"),
+    resolve: {
+      alias: {}
+    },
+    server: {
+        port: 8080,
+        host: true
+    },
+    build: {
+        outDir: "./dist"
+    },
     plugins: []
 })
