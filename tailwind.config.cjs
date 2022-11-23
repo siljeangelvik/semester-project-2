2281068355d4c2b349f7ec859dcfd690d4370700
silje <angelviksilje@gmail.com>
2022-11-23 13:07:13 +0100

    edited a small detail to check if deployment to GitHub Pages work..

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,26 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'secondary': {
-          '50': '#f9f6fa',
-          '100': '#f3ecf6',
-          '200': '#e2d0e8',
-          '300': '#d0b3db',
-          '400': '#ad7abf',
-          '500': '#8a41a4',
-          '600': '#7c3b94',
-          '700': '#68317b',
-          '800': '#532762',
-          '900': '#442050'
-        }
-      }
+    content: [
+        "./dist/**/*.{js,css}",
+        "./dist/index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'secondary': {
+                    '50': '#f9f6fa',
+                    '100': '#f3ecf6',
+                    '200': '#e2d0e8',
+                    '300': '#d0b3db',
+                    '400': '#ad7abf',
+                    '500': '#8a41a4',
+                    '600': '#7c3b94',
+                    '700': '#68317b',
+                    '800': '#532762',
+                    '900': '#442050'
+                }
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
