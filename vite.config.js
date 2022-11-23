1f1bd1ab0365e3939264c49ac35a88902eda957f
silje <angelviksilje@gmail.com>
2022-11-23 18:58:38 +0100

    testing some things

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/semester-project-2/',
+    base: '/',
     root: path.resolve(__dirname, "./"),
     resolve: {
       alias: {}
